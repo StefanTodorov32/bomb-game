38eafbca5bba53158c0f2f142c509e4b24c7d9c0
StefanTodorov32 <bgtrak@gmail.com>
2023-05-20 19:20:03 +0300

    fixed bug with the createRandomArray

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,24 @@
 import {IBombBox} from "./types";
 
 export function createRandomArray(bombs: number): IBombBox[] {
-	const array: IBombBox[] = [];
+    const array: IBombBox[] = [];
 
-	const bombIndices: number[] = [];
-	while (bombIndices.length < bombs) {
-		const index = Math.floor(Math.random() * 9);
-		if (!bombIndices.includes(index)) {
-			bombIndices.push(index);
-		}
-	}
-	for (let i = 0; i < 18; i++) {
-		const item: IBombBox = {
-			id: i,
-			isBomb: bombIndices.includes(i),
-			marked: false,
-		};
-		array.push(item);
-	}
+    const bombIndices: number[] = [];
+    while (bombIndices.length < bombs) {
+        const index = Math.floor(Math.random() * 18);
+        if (!bombIndices.includes(index)) {
+            bombIndices.push(index);
+        }
+    }
 
-	return array;
+    for (let i = 0; i < 18; i++) {
+        const item: IBombBox = {
+            id: i,
+            isBomb: bombIndices.includes(i),
+            marked: false,
+        };
+        array.push(item);
+    }
+
+    return array;
 }
